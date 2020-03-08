845eaab3cc73d904dae421c012de29567b5bf7de
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2020-03-08 13:47:33 +0100

    Add provider info and secrets

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 terraform {
   required_version = "~>0.12"
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate71ac2932"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  version = "=2.0.0"
+  version         = "=2.0.0"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+
   features {}
 }
 
