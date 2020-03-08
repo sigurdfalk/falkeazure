17ee086fabca5a7a867fa5f62819cf44acc3716e
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2020-03-08 14:01:38 +0100

    tf env vars

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
+variable "tenant_id" {}
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
-variable "tenant_id" {}
 
 terraform {
   required_version = "~>0.12"
@@ -16,10 +16,10 @@ terraform {
 
 provider "azurerm" {
   version         = "=2.0.0"
-  client_id       = var.client_id
-  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 
   features {}
 }
